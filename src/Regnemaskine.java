bc82640336738dcaa460a2f6be92b1edcaba2538
Joakim Anker Kruse <joakimkruse@hotmail.com>
2021-09-20 15:56:56 +0200

    Initial commit

---
+++ src/Regnemaskine.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Regnemaskine {
+    public static void main(String[] args) {
+
+        float num1;
+        float num2;
+        char operator;
+        double answer = 0.0;
+
+        Scanner scanObject = new Scanner(System.in);
+
+        System.out.println("Please enter first number:");
+        num1 = scanObject.nextFloat();
+        System.out.println("Please enter second number:");
+        num2 = scanObject.nextFloat();
+        System.out.println("What operation?");
+        operator = scanObject.next().charAt(0);
+
+        switch (operator) {
+            case '+': answer = num1 + num2;
+            break;
+            case '-': answer = num1 - num2;
+            break;
+            case '*': answer = num1 * num2;
+            break;
+            case '/': answer = num1 / num2;
+            break;
+        }
+        System.out.println(num1+" "+operator+" "+num2+" = "+answer);
+        }
+    }
